3e26ebde319eef0fb6412e2cd2ddbf062cb4f54b
Lukáš Stankovič <lukas.stankovic@shopsys.com>
2020-02-28 20:39:53 +0100

    Request handler in threads

--- app/src/main/java/com/stankovic/lukas/httpserver/Controller/IBaseController.java
+++ app/src/main/java/com/stankovic/lukas/httpserver/Controller/IBaseController.java
@@ -2,7 +2,10 @@ package com.stankovic.lukas.httpserver.Controller;
 
 import com.stankovic.lukas.httpserver.Http.Response.Response;
 
+import java.io.IOException;
+
 public interface IBaseController {
 
+    public void render() throws IOException;
 
 }
