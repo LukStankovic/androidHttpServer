b6e65988d446ead9a31c93fb9651a0a558fbebf2
Lukáš Stankovič <lukas.stankovic@shopsys.com>
2020-03-02 12:37:04 +0100

    !fixup filereader typo

--- app/src/main/java/com/stankovic/lukas/httpserver/Controller/IBaseController.java
+++ app/src/main/java/com/stankovic/lukas/httpserver/Controller/IBaseController.java
@@ -1,11 +1,9 @@
 package com.stankovic.lukas.httpserver.Controller;
 
-import com.stankovic.lukas.httpserver.Http.Response.Response;
-
 import java.io.IOException;
 
 public interface IBaseController {
 
-    public void render() throws IOException;
+    void render() throws IOException;
 
 }
